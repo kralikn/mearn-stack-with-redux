bb5b76ba51cc6d50582a081d9bbfdc484096a84e
Králik Norbert <kraaliknorbert@gmail.com>
2021-03-26 15:39:39 +0100

    basic redux

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.scss';
+import { Provider } from 'react-redux'
+import store from './redux/store'
+
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ frontend/src/redux/rootReducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+// import topicReducer from './topic/topicReducer'
+
+const rootReducer = combineReducers({
+  // topics: topicReducer
+})
+
+export default rootReducer
